adeadc06a0647625bec87c71bbab330b296d2e50
faeshal <faeshal111@gmail.com>
2019-08-30 11:12:21 +0700

    add pagination

--- controller/admin.js
+++ controller/admin.js
@@ -1,16 +1,33 @@
 const Product = require("../models/product");
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render("index", {
         prods: products,
-        pageTitle: "Admin Products",
+        isAuthenticated: req.session.isLoggedIn,
         path: "/",
-        isAuthenticated: req.session.isLoggedIn
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getAddProduct = (req, res, next) => {
